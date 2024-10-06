74d525ad6bf402d338cf4281af5b0fd25eab152b
shivani <shivani@localhost>
2024-10-06 16:46:45 -0700

    Created getter and setter methods in User class

--- CSE360GroupProject/src/com/yourdomain/cse360groupproject/User.java
+++ CSE360GroupProject/src/com/yourdomain/cse360groupproject/User.java
@@ -11,16 +11,18 @@ public class User {
 	int password;
 	boolean isNewUser;
 	boolean forgotPassword;
-	boolean tempPassword;
 	
 	//default constructor
 	public User() {
 		firstName = "";
 		preferredName = "";
 		middleName = "";
+		lastName = "";
 		emailAddress = "";
 		username = "";
 		password = 0;
+		isNewUser = true;
+		forgotPassword = false;
 	}
 	
 	//overload constructor
@@ -28,10 +30,72 @@ public class User {
 		this.firstName = firstName;
 		this.preferredName = preferredName;
 		this.middleName = middleName;
+		this.lastName = lastName;
 		this.emailAddress = emailAddress;
 		this.username = username;
 		this.password = password;
+		isNewUser = true;
+		forgotPassword = false;
 	}
 	
+	//first name getter
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	//first name setter
+	public void setFirstName(String newFirstName) {
+		firstName = newFirstName;
+	}
+	
+	//preferred name getter
+	public String getPreferredName() {
+		return preferredName;
+	}
+	
+	//preferred name setter
+	public void setPreferredName(String newPreferredName) {
+		preferredName = newPreferredName;
+	}
+	
+	//middle name getter
+	public String getMiddleName() {
+		return middleName;
+	}
+		
+	//middle name setter
+	public void setMiddleName(String newMiddleName) {
+		middleName = newMiddleName;
+	}
+	
+	//last name getter
+	public String getLastName() {
+		return lastName;
+	}
+			
+	//last name setter
+	public void setLastName(String newLastName) {
+		lastName = newLastName;
+	}
+	
+	//email address getter
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+				
+	//email address setter
+	public void setEmailAddress(String newEmailAddress) {
+		emailAddress = newEmailAddress;
+	}
+	
+	//username getter
+	public String getUsername() {
+		return username;
+	}
+					
+	//username setter
+	public void setUsername(String newUsername) {
+		username = newUsername;
+	}
 	
 }
