70ab33862c64fd125d198bba3ee95afff89789f7
kirraj725 <kirtiraj1802@gmail.com>
2024-10-08 14:04:23 -0700

    adding to admin; need to add multiple role function for users

--- CSE360GroupProject/src/com/yourdomain/cse360groupproject/Administrator.java
+++ CSE360GroupProject/src/com/yourdomain/cse360groupproject/Administrator.java
@@ -5,10 +5,16 @@ import java.util.ArrayList;
 
 
 public class Administrator extends User{
-	
+	public String roleInput = "";	//
 	//create arraylist to store users in
-	private List <User> userlist = new ArrayList();
+	public List <User> userlist = new ArrayList<>();
 	
+	
+	//constructor
+    public Administrator(String firstName, String preferredName, String middleName, String lastName, String emailAddress, String username, int password) {
+        super(firstName, preferredName, middleName, lastName, emailAddress, username, password);
+        roleInput = "Administrator";
+    }
 	
 	public void inviteUser(User user) {
 		//generate one time code for new user
@@ -19,14 +25,20 @@ public class Administrator extends User{
 		
 		System.out.print("Select the roles for the new user: ");
 		//set user input equal to roleInput string
-		//string roleInput = 
+		//string roleInput = "";
 		/*switch(roleInput) {
-		//need to add button control stuff here
+		need to add button control stuff here
 			case "Student":
+				roleInput = "Student";
+				newUser.setRole(roleInput);
 				
 			case "Instructor":
+				roleInput = "Instructor";
+				newUser.setRole(roleInput);
 				
 			case "Administrator":
+				roleInput = "Administrator";
+				newUser.setRole(roleInput);
 		
 		}*/
 		
@@ -34,33 +46,60 @@ public class Administrator extends User{
 		
 	}
 	
+	
 	public void resetUserPassword(User user) {
-		//
+		
 		
 	}
 	
-	public void deleteUserAccount(User user) {
+	public void deleteUserAccount(User user, String roleInput) {
 		
 		
 		System.out.println("Are you sure?");
+		
+		if(roleInput == "Yes") {
+			
+		}
 		//add in button control here for Yes and No options
 		
 		
-	}
-	
-	public void listUserAccounts() {
 		
 	}
 	
-	public void addUserRole() {
+	public void listUserAccounts(List userlist) {
+		//System.out.print(userlist);
+		//for(User user : userlist) {
+			
+		//}
+		
 		
 	}
 	
-	public void deleteUserRole() {
+	public void addUserRole(String roleInput) {
+		switch(roleInput) {
+			case("Student"):
+				
+		}
+		
+		
+	}
+	
+	public void deleteUserRole(String roleInput, String email) {
+		switch(roleInput) {
+			case("Student"):
+			
+				
+			case("Instuctor"):
+			
+			case("Administrator"):
+			
+		}
+				
 		
 	}
 	
 	public void logout() {
+		
 		
 	}
 	
