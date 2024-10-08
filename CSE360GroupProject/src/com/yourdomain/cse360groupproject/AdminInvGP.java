e42a9b55d3523e7e9f56888421cad6f7efedafa4
arya-chak <125517854+arya-chak@users.noreply.github.com>
2024-10-08 16:59:16 -0700

    Delete CSE360GroupProject/src/com/yourdomain/cse360groupproject/AdminInvGP.java
    deletin this file

--- CSE360GroupProject/src/com/yourdomain/cse360groupproject/AdminInvGP.java
+++
@@ -1,9 +0,0 @@
-package groupProjectScreens;
-
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class AdminInvGP extends Application {
-   public void start(Stage primaryStage) {
-   }
-}
