dce411b1763180b8a00df476c6460381515cd359
arya-chak <125517854+arya-chak@users.noreply.github.com>
2024-10-08 16:01:09 -0700

    admin invitation and logout screen
    admin invitation is unfinished

---
+++ CSE360GroupProject/src/com/yourdomain/cse360groupproject/AdminInvGP.java
@@ -0,0 +1,9 @@
+package groupProjectScreens;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AdminInvGP extends Application {
+   public void start(Stage primaryStage) {
+   }
+}
