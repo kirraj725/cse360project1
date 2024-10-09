ddeba4035aa1169cc5dfdfeccf9a51090148ef05
sahmvn <sahmanalsubaie50@gmail.com>
2024-10-08 20:00:26 -0700

    Add files via upload

--- CSE360GroupProject/src/com/yourdomain/cse360groupproject/AddRemoveRolegp.java
+++ CSE360GroupProject/src/com/yourdomain/cse360groupproject/AddRemoveRolegp.java
@@ -1,26 +1,28 @@
-package application;
-	
+package groupProjectScreens;
+
+import com.yourdomain.cse360groupproject.Button;
+import com.yourdomain.cse360groupproject.HBox;
+import com.yourdomain.cse360groupproject.Label;
+import com.yourdomain.cse360groupproject.VBox;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
 
 public class AddRemoveRolegp extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-}
+   public void start(Stage primaryStage) {
+	   Label headingLabel = new Label("Pick a Role");
+	   Button AdminButton = new Button("Admin");
+	   Button instructorButton = new Button("Instructor");
+	   Button StudentButton = new Button("Student");
+	      Button backButton = new Button("Back");
+	      HBox buttonBox = new HBox(10);
+	      buttonBox.setAlignment(Pos.CENTER);
+	      buttonBox.getChildren().addAll(AdminButton, instructorButton, StudentButton);
+	      VBox vbox = new VBox(20);
+	      vbox.setAlignment(Pos.CENTER);
+	      vbox.getChildren().addAll(headingLabel, buttonBox, backButton);
+          
+          
+    }
+	   
+   }
