3ab868e671f1bb4bbe0abc01ee81a43958035565
sahmvn <sahmanalsubaie50@gmail.com>
2024-10-09 21:31:16 -0700

    Update RPWTest1.java

--- CSE360GroupProject/src/com/yourdomain/cse360groupproject/RPWTest1.java
+++ CSE360GroupProject/src/com/yourdomain/cse360groupproject/RPWTest1.java
@@ -1,3 +1,4 @@
+//This is the screen where admin sends an email to help users reset their passwords
 package TEST1Phase1;
 
 import javafx.application.Application;
@@ -12,23 +13,27 @@ import javafx.stage.Stage;
 
 public class RPWTest1 extends Application {
    public void start(Stage primaryStage) {
+      //Creating an instruction label
       Label instructionLabel = new Label("Enter E-mail");
+      //Creating a label and textfield for email
       Label emailLabel = new Label("Email:");
       TextField emailField = new TextField();
+      //HBox to hold email label and textfield together
       HBox emailBox = new HBox(10);
       emailBox.setAlignment(Pos.CENTER);
       emailBox.getChildren().addAll(emailLabel, emailField);
       Button sendButton = new Button("Send");
-      // add action to send email
+      // Button to go back to the account list screen
       Button backButton = new Button("Back");
-      // add action to send back to the account list
       backButton.setOnAction(e -> {
   	  		AccountListTest1 AccList = new AccountListTest1();
     		AccList.start(primaryStage);	  
       });
+      //Used to hold everything on top of eachother
       VBox vbox = new VBox(20);
       vbox.setAlignment(Pos.CENTER);
       vbox.getChildren().addAll(instructionLabel, emailBox, sendButton, backButton);
+      //Showing stage and setting scene
       Scene scene = new Scene(vbox, 400, 300);
       primaryStage.setTitle("Reset Password Page");
       primaryStage.setScene(scene);
